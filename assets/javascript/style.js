8a1db4bfd9040eb37cc3eb6058f0b88138ef1a77
Farhan Solaiman <40680452+FarhanSolaiman@users.noreply.github.com>
2018-10-03 14:46:23 +0800

    Update style.js

--- assets/javascript/style.js
+++ assets/javascript/style.js
@@ -1,14 +1,52 @@
-		$(window).scroll(function() {
-		    if ($(this).scrollTop() >= 768) {      
-		        $('#return-to-top').fadeIn(400);    // Fade in the arrow
-		    } else {
-		        $('#return-to-top').fadeOut(10);   // Else fade out the arrow
-		    }
-		});
-		$('#return-to-top').click(function() {      // When arrow is clicked
-		    $('body,html').animate({
-		        scrollTop : 0                       // Scroll to top of body
-		    }, 500);
-		});
+$.ajaxSetup({
+  headers: {
+    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+  }
+});
 
-		
+$(window).scroll(function() {
+	if ($(this).scrollTop() >= 768) {      
+        $('#return-to-top').fadeIn(400);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(10);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+	$('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
+
+$(document).on('click','#sendbtn', function () {
+	let name = $('#name').val();
+	let email = $('#email').val();
+	let number = $('#number').val();
+	let message = $('#message').val();
+
+	if (name.length == 0) {
+	    $('#success').show();
+		$('#success').css('color','#E24343');
+		$('#success').html('Please fill up the required fields.');
+		$('#success').delay(1500).fadeOut('slow');
+	} else if(email.length == 0) {
+	    $('#success').show();
+		$('#success').css('color','#E24343');
+		$('#success').html('Please fill up the required fields.');
+		$('#success').delay(1500).fadeOut('slow');
+	} else if(message.length == 0) {
+	    $('#success').show();
+		$('#success').css('color','#E24343');
+		$('#success').html('Please fill up the required fields.');
+		$('#success').delay(1500).fadeOut('slow');
+	} else {
+		$('#success').css('color','#58CB34');
+		$('#success').html('Your message is successfully forwarded to my email. Thaank you.');
+		$('#success').delay(3000).fadeOut('slow');
+		$('#name').val('');
+		$('#email').val('');
+		$('#number').val('');
+		$('#message').val('');
+			}	
+		})
+	}
+});
